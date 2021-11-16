41adcd7ea6bba4d06e98c2ccfc8a46c6de3d69e9
danilsyah <danilsyaharihardjo@gmail.com>
2021-11-16 09:07:21 +0700

    channel sebagai parameter

--- channel_test.go
+++ channel_test.go
@@ -25,3 +25,25 @@ func TestCreateChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+// channel sebagai parameter
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "haykal dafiansyah"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	// data diterima oleh channel
+	go GiveMeResponse(channel)
+	fmt.Println("selesai mengirim data ke channel")
+
+	// data channel di terima oleh variabel nama
+	nama := <-channel
+	fmt.Println(nama)
+
+	time.Sleep(5 * time.Second)
+
+}
