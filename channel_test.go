016fa37a82eb03db9fb9fb7daa697d8c17e8da93
danilsyah <danilsyaharihardjo@gmail.com>
2021-11-17 09:57:43 +0700

    belajar default select channel

--- channel_test.go
+++ channel_test.go
@@ -125,11 +125,38 @@ func TestSelectChannel(t *testing.T) {
 	for {
 		select {
 		case data := <-channel1:
-			fmt.Println("Data dari channell 1 ", data)
+			fmt.Println("Data dari channel 1 ", data)
 			counter++
 		case data := <-channel2:
-			fmt.Println("Dataa dari channel 2 ", data)
+			fmt.Println("Data dari channel 2 ", data)
 			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1 ", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2 ", data)
+			counter++
+		default:
+			fmt.Println("Menunggu Data")
 		}
 
 		if counter == 2 {
